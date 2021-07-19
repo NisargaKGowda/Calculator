09d3574deb372c468faf6de84da55690bd13a13c
NisargaKGowda <74409964+NisargaKGowda@users.noreply.github.com>
2021-07-19 19:44:06 +0530

    Add files via upload

---
+++ R/calci.R
@@ -0,0 +1,37 @@
+#'creates a function to add two numbers
+#'
+#'@param x is a number
+#'@param y  is a number
+#'@return the sum
+#'@export
+add <- function(x, y) {
+  return(x + y)
+}
+#'creates a function to subtract two numbers
+#'
+#'@param x is a number
+#'@param y  is a number
+#'@return the difference
+#'@export
+subtract <- function(x, y) {
+  return(x - y)
+}
+#'creates a function to get product of two numbers
+#'
+#'@param x is a number
+#'@param y  is a number
+#'@return the product
+#'@export
+multiply <- function(x, y) {
+  return(x * y)
+}
+#'creates a function to divide two numbers
+#'
+#'@param x is a number
+#'@param y  is a number
+#'@return the divisor
+#'@export
+divide <- function(x, y) {
+  return(x / y)
+}
+
